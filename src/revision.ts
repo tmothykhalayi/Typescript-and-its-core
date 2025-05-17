81db1006e24b767a333871bd126e8487fb73fb6c
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-05-17 21:43:31 +0300

    update

--- src/revision.ts
+++ src/revision.ts
@@ -41,10 +41,33 @@
 
 // let price: number[] = [100, 200, 300, 800];
 // price.push(1000);
-let Car :{isElectric:boolean,model:string,year:number,name:string , brand:string}= {
-    isElectric:true,
-    model:"lexus",
-    year:2023,
-    name:"timothy",
-    brand:"toyota"
-};
+// let Car :{isElectric:boolean,model:string,year:number,name:string , brand:string}= {
+//     isElectric:true,
+//     model:"lexus",
+//     year:2023,
+//     name:"timothy",
+//     brand:"toyota"
+// };
+
+// console.log(Car);
+//functions
+// function sayHi(name:string){
+//     console.log('Hello , ${name.toUpperCase()}');
+// }
+// sayHi("Timothy");
+//2.functions
+// function calculateDiscount(price:number){
+//     const hasDiscount:boolean =true;
+//     if (hasDiscount){
+//         return price*0.1;
+//     }
+// return price*0.2;
+// }
+// const finalDiscount = calculateDiscount(200);
+// console.log(`The final discount is ${finalDiscount}`);
+
+//3.function 
+function addthreeNumbers(num1: number, num2: number, num3: number): number {
+    return num1 + num2 + num3;
+}
+console.log(addthreeNumbers(5,6 ,20));

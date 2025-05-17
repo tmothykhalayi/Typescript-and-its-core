e96a37722881a70c2709e9ea94f5deffe4f44aeb
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-05-17 13:51:13 +0300

    update

---
+++ src/revision.ts
@@ -0,0 +1,12 @@
+console.log("turtorial file");
+interface Admin {
+    id: number;
+    name: string;
+    email: string;
+}
+let admin:Admin ={
+    id: 1,
+    name: "John Doe",
+    email: "timothykhalayi@gmail.com"
+}
+console.log(admin);
